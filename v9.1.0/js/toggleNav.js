55386fdf19ebcee04ce2bc7c775be8505880914e
billie-jean <root@gooddata.com>
2023-09-08 14:40:23 +0200

    Create a new api docs version 9.1.0

---
+++ v9.1.0/js/toggleNav.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var navToggler = document.getElementById("navToggler");
+
+    if (navToggler) {
+        var bodyClasses = document.body.classList;
+        var NAV_OPEN = "toggleNavOpen";
+
+        navToggler.addEventListener("click", function () {
+            if (bodyClasses.contains(NAV_OPEN)) {
+                bodyClasses.remove(NAV_OPEN);
+            } else {
+                bodyClasses.add(NAV_OPEN);
+            }
+        });
+    }
+
+    document.querySelector("header .logo").parentNode.href = "https://www.gooddata.com/";
+});
